6ff9cbea835c1884ec74df9d5b7d9e34faa03f4e
JUGADORUS <savelilya07@gmail.com>
2024-06-15 20:59:23 +0500

    fix

--- Assets/Scripts/Coffee.cs
+++ Assets/Scripts/Coffee.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Coffee : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter(Collision collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(collision.gameObject.TryGetComponent(out Mover mover))
+        {
+            
+        }
     }
 }
